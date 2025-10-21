a6a893b3d9173945b00d99debdf36dfcfc9456be
Mukul Kushwaha <mukul.kushwahaa@gmail.com>
2025-10-21 10:32:59 +0530

    fixes 2.2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { IconProvider } from "./contexts/IconContext";
 import HomePage from "./pages/HomePage";
@@ -7,22 +7,39 @@ import LobbyPage from "./pages/LobbyPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 
-function App() {
-  return (
-    <AuthProvider>
-      <IconProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/lobby" element={<LobbyPage />} />
-            <Route path="/game" element={<GamePage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-          </Routes>
-        </Router>
-      </IconProvider>
-    </AuthProvider>
-  );
-}
+const AppLayout = () => (
+  <AuthProvider>
+    <IconProvider>
+      <Outlet />
+    </IconProvider>
+  </AuthProvider>
+);
 
-export default App;
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/lobby",
+        element: <LobbyPage />,
+      },
+      {
+        path: "/game",
+        element: <GamePage />,
+      },
+      {
+        path: "/profile",
+        element: <ProfilePage />,
+      },
+    ],
+  },
+]);
